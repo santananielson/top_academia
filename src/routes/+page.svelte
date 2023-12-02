015466ac96414e7cd099efc1e00fee580a9380be
santananielson <nielsonsantana2@gmail.com>
2023-12-02 00:12:25 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<h2>Nielson</h2>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<style>
+    h1 {
+        color: var(--feat);
+    }
+</style>
