4081c3d300afdff54d2bc43ba9113357fd44ecc1
santananielson <nielsonsantana2@gmail.com>
2023-12-09 14:15:51 -0300

    Otimização: indicar qual é a página atual no menu

--- src/lib/Credists.svelte
+++ src/lib/Credists.svelte
@@ -1,17 +1,22 @@
 <div class="credits">
     <div class="credits__wrapper wrap">
         <span>© Top Academia, 2023</span>
-    </div>
+        <span>Desenvolvido por <a href="https://github.com/santananielson" style="color:#FF3E00; text-decoration:none;" target="_blank">Nielson Santana</a></span>
+    </div>        
 </div>
-
 
 <style>
     .credits {
         margin-top: 60px;
     }
+    .credits__wrapper {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
     .credits__wrapper::before {
         content: '';
-        display: block;
+        display: flex;
         width: 100%;
         height: 1px;
         background-color: var(--subtle);
@@ -21,5 +26,17 @@
         font-size: calc(14 / 16 * 1rem);
         padding-top: 24px;
         padding-bottom: 24px;
+    }
+
+    /* Adicione a media query para telas menores que 800px */
+    @media (max-width: 800px) {
+        .credits__wrapper {
+            flex-direction: column; /* Exibe os itens um abaixo do outro */
+            align-items: center; /* Centraliza os itens horizontalmente */
+        }
+        /* Ajuste o espaçamento inferior (margin-bottom) nas spans */
+        span {
+         margin-bottom: -30px; /* Ajuste conforme necessário */
+        }
     }
 </style>
