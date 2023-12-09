19c8d492b01e32a8f0ed2cdcb344d61bded22cff
santananielson <nielsonsantana2@gmail.com>
2023-12-09 14:37:58 -0300

    Ajustes finais

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <Intro
     title="Você está pronto<br> para o desafio?"
-    content="A Academia Local está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance."
+    content="A Top Academia está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance."
     btn="Sobre"
     href="/sobre"
     img="musculacao"
