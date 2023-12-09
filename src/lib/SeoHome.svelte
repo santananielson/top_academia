10522714112a7324429dc0195756b2eabafc843e
santananielson <nielsonsantana2@gmail.com>
2023-12-09 17:20:40 -0300

    open graph meta tags

--- src/lib/SeoHome.svelte
+++ src/lib/SeoHome.svelte
@@ -1,4 +1,5 @@
 <script>
+    import img from '$lib/assets/musculacao.avif'
     export let description;
 
     let siteName = 'Top Academia';
@@ -8,4 +9,10 @@
 <svelte:head>
     <title>{siteName}{separator}A Academia pertinho de você!</title>
     <meta name="description" content="{description}">
+
+    <!-- Open Graph meta tags -->
+    <meta property="og:title" content="A Academia pertinho de você!"/>
+    <meta property="og:description" content="{description}"/>
+    <meta property="og:image" content="{img}"/>
+
 </svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script>
     import Intro from '$lib/Intro.svelte';
 	import SeoHome from '$lib/SeoHome.svelte';
+
 </script>
 
 
-<SeoHome
-
-/>
+<SeoHome/>
 <Intro
     title="Você está pronto<br> para o desafio?"
     content="A Top Academia está pronta para te ajudar a chegar mais longe e alcançar sua melhor performance."
